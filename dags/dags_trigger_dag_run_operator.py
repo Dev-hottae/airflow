12a35d46f5d6c53d8ed41e94972720ed0a429767
hottae <hottae0805@gmail.com>
2024-11-21 19:24:32 +0900

    uploads trigger dags

---
+++ dags/dags_trigger_dag_run_operator.py
@@ -0,0 +1,34 @@
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+
+with DAG(
+    dag_id="dags_trigger_dag_run_operator",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2023, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["example", "example2"],
+) as dag:
+    
+    start_task = BashOperator(
+        task_id='start_task',
+        bash_command='echo "start!"'
+    )
+
+    trigger_dag_task = TriggerDagRunOperator(
+        task_id='trigger_dag_task',
+        trigger_dag_id='dags_python_operator',
+        trigger_run_id=None,
+        execution_date='{{ data_interval_start }}',
+        reset_dag_run=True,
+        wait_for_completion=False,
+        poke_interval=60,
+        allowed_states=['success'],
+        failed_states=None
+    )
+    
+    start_task >> trigger_dag_task
